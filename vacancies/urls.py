86880359268d0856ab043148df9fab52f1932fc4
Julia <julii.hess@gmail.com>
2021-01-17 13:57:50 +0300

    Добавлены тыкабельные ссылки

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -10,7 +10,7 @@ from vacancies.views import VacancyListView
 urlpatterns = [
     path('', MainListView.as_view(), name='main'),
     path('vacancies/', AllVacanciesListView.as_view(), name='all_vacancies'),
-    path('vacancies/cat/<str:specialization_id>/', SpecializationListView.as_view()),
-    path('companies/<int:company_id>/', CompanyListView.as_view()),
-    path('vacancies/<int:vacancy_id>/', VacancyListView.as_view()),
+    path('vacancies/cat/<str:specialization_id>/', SpecializationListView.as_view(), name='specialization'),
+    path('companies/<int:company_id>/', CompanyListView.as_view(), name='company'),
+    path('vacancies/<int:vacancy_id>/', VacancyListView.as_view(), name='vacancy'),
 ]
