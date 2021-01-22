517cb2572eb5a80fac5609a641ea67ea2977be6e
Julia <julii.hess@gmail.com>
2021-01-22 07:17:29 +0300

    Добавлена модель Application, в модели Company добавлено поле owner, мелкие изменения

--- vacancies/models.py
+++ vacancies/models.py
@@ -1,20 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
+User = get_user_model()
+
+
 class Company(models.Model):
-    title = models.CharField(max_length=64)
-    location = models.CharField(max_length=64)
+    title = models.CharField(max_length=120)
+    location = models.CharField(max_length=120)
     logo = models.URLField(default='https://place-hold.it/100x60')
     description = models.TextField()
     employee_count = models.IntegerField()
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
 
 
 class Specialty(models.Model):
-    code = models.CharField(max_length=64)
-    title = models.CharField(max_length=64)
+    code = models.CharField(max_length=120)
+    title = models.CharField(max_length=120)
     picture = models.URLField(default='https://place-hold.it/100x60')
 
     def __str__(self):
@@ -22,7 +27,7 @@ class Specialty(models.Model):
 
 
 class Vacancy(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.CharField(max_length=120)
     specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE, related_name='vacancies')
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='vacancies')
     skills = models.TextField()
@@ -33,3 +38,11 @@ class Vacancy(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Application(models.Model):
+    written_username = models.CharField(max_length=120)
+    written_phone = models.CharField(max_length=120)
+    written_cover_letter = models.TextField()
+    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, related_name='applications')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='applications')
