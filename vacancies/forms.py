9f1ef3ed29d2eeca2f42a00d581fefde5d877740
Julia <julii.hess@gmail.com>
2021-01-25 00:48:24 +0300

    Разработка формы регистрации

--- vacancies/forms.py
+++ vacancies/forms.py
@@ -6,15 +6,18 @@ from crispy_forms.layout import Submit
 
 
 class SignUpForm(UserCreationForm):
-    login = forms.CharField(min_length=3)
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    password = forms.PasswordInput()
+    login = forms.CharField(min_length=3, label='Логин')
+    first_name = forms.CharField(label='Имя')
+    last_name = forms.CharField(label='Фамилия')
     class Meta:
         model = User
-        fields = ('login', 'first_name', 'last_name', 'password')
+        fields = ('login', 'first_name', 'last_name')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Зарегистрироваться'))
+
+        self.helper.label_class = 'text-muted'
+
